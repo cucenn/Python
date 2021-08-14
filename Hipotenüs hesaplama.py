f267bf8dbaa77013fddefd200ccfe860ac206fd5
cucenn <87239517+cucenn@users.noreply.github.com>
2021-08-14 18:42:37 +0300

    Add files via upload

---
+++ Hipotenüs hesaplama.py
@@ -0,0 +1,4 @@
+a= float(input("üçgenin bir kenar uzunluğunu girin:"))
+b=float(input("üçgenin ikinci kenar uzunluğunu girin:"))
+hipotenüs= float((a**2+b**2)**0.5)
+print("iki kısa kenar ölçüsü {} ve {} olan üçgenin hipotenüs uzunluğu = {}'dır. ".format(a,b,hipotenüs))
---
+++ Vücut Kitle İndeksi.py
@@ -0,0 +1,5 @@
+
+Kilo= int(input("kilonuzu giriniz:"))
+Boy =float(input("boyunuzu giriniz:"))
+İndex=float(Kilo%Boy**2)
+print("{}%{}*{}={}'dir.".format(Kilo,Boy,Boy,İndex))

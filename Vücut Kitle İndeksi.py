b308dde27ad4ca4eecde6d4994fc4f01c09584c9
Nilüfer Ela Cüce <87239517+cucenn@users.noreply.github.com>
2021-08-15 16:12:07 +0300

    Update Vücut Kitle İndeksi.py

--- Vücut Kitle İndeksi.py
+++ Vücut Kitle İndeksi.py
@@ -1,5 +1,14 @@
-
-Kilo= int(input("kilonuzu giriniz:"))
-Boy =float(input("boyunuzu giriniz:"))
-İndex=float(Kilo%Boy**2)
-print("{}%{}*{}={}'dir.".format(Kilo,Boy,Boy,İndex))
+print("***Beden Kitle Endeksi Hesaplama.***")
+boy = float(input("boyunuzu giriniz:"))
+kilo = int(input("kilonuzu giriniz:"))
+islem = int(kilo / (boy**2))
+if (islem>30):
+    print("Obez")
+elif(islem<=30 and islem>25):
+    print("Fazla Kilolu")
+elif(islem<=25 and islem>18.5):
+    print("Normal")
+elif(islem<=18.5 and islem>16):
+    print("Zayıf")
+else:
+    print("Acından Ölmüş:)")
